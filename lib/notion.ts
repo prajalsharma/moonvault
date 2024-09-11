d8ca9f185ecac77d5882e72f58b6e46b7b3efd27
gulshanpr <gulshankumarprasad04@gmail.com>
2024-09-11 20:45:31 +0530

    build fix

--- lib/notion.ts
+++ lib/notion.ts
@@ -3,31 +3,40 @@ import { Client } from '@notionhq/client';
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 export async function getBlogPosts() {
-  const databaseId = process.env.NOTION_DATABASE_ID || '';
-  const response = await notion.databases.query({
-    database_id: databaseId,
-  });
+  try {
+    const databaseId = process.env.NOTION_DATABASE_ID;
+    if (!databaseId) {
+      throw new Error('NOTION_DATABASE_ID is not defined');
+    }
+
+    const response = await notion.databases.query({
+      database_id: databaseId,
+    });
   
-  return response.results.map((page: any) => ({
-    id: page.id,
-    role: page.properties['Role'].title[0]?.text.content || '',
-    location: page.properties['Location'].rich_text[0]?.text.content || 'N/A',
-    jobDescription: page.properties['Job Description']?.rich_text?.map((richText: any) => {
-      if (richText.type === 'text') {
-        const { content, link } = richText.text;
-        return link ? link.url : content;
-      }
-      return ''; 
-    }).join('') || 'N/A',
-    company: page.properties['Project'].rich_text[0]?.text.content || 'N/A',
-    type: page.properties['Job Type'].rich_text[0]?.text.content || 'N/A',
-    logo: page.properties['Image']?.rich_text?.map((richText: any) => {
-      if (richText.type === 'text') {
-        const { content, link } = richText.text;
-        return link ? link.url : content;
-      }
-      return ''; 
-    }).join(''),
-    // logo: page.properties['Image'].files?.file?.url || page.properties['Image'].files?.external?.url,
-  }));
+    return response.results.map((page: any) => ({
+      id: page.id,
+      role: page.properties['Role'].title[0]?.text.content || '',
+      location: page.properties['Location'].rich_text[0]?.text.content || 'N/A',
+      jobDescription: page.properties['Job Description']?.rich_text?.map((richText: any) => {
+        if (richText.type === 'text') {
+          const { content, link } = richText.text;
+          return link ? link.url : content;
+        }
+        return ''; 
+      }).join('') || 'N/A',
+      company: page.properties['Project'].rich_text[0]?.text.content || 'N/A',
+      type: page.properties['Job Type'].rich_text[0]?.text.content || 'N/A',
+      logo: page.properties['Image']?.rich_text?.map((richText: any) => {
+        if (richText.type === 'text') {
+          const { content, link } = richText.text;
+          return link ? link.url : content;
+        }
+        return ''; 
+      }).join(''),
+      // logo: page.properties['Image'].files?.file?.url || page.properties['Image'].files?.external?.url,
+    }));
+  } catch (error) {
+    console.error('Error fetching blog posts:', error);
+    throw error;
+  }
 }

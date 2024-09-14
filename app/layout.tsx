58daaff22ac66e2c30dc46ed42398d714511d6bb
Aditya Phasu <adityaphasu919@gmail.com>
2024-09-14 10:42:22 +0530

    add next-themes and theme-provider for default light theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import Header from "@/components/Header";
 import SecondaryNavbar from "@/components/SecondaryNavbar";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const plus_jakarata_sans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
@@ -18,16 +19,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${plus_jakarata_sans.className}`}>
-        <Header />
-        <div>
-          <section className="main-section h-screen flex justify-center items-center bg-[#eaf2ff]">
-            <h1 className="text-xl md:text-4xl font-bold">EigenJobs(this is for future stuff)</h1>
-          </section>
-          <SecondaryNavbar />
-        </div>
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
+          <Header />
+          <div>
+            <section className="main-section h-screen flex justify-center items-center bg-[#eaf2ff]">
+              <h1 className="text-xl md:text-4xl font-bold">EigenJobs(this is for future stuff)</h1>
+            </section>
+            <SecondaryNavbar />
+          </div>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );

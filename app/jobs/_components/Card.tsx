5109432e3df1f1de90e3c95fe55c38c87915a318
Aditya Phasu <adityaphasu919@gmail.com>
2024-09-14 10:43:10 +0530

    fix and handle missing job logo , category and location

--- app/jobs/_components/Card.tsx
+++ app/jobs/_components/Card.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import { MapPin } from "lucide-react";
+import { ImageOff, MapPin } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -32,24 +32,28 @@ const Card = ({ job }: JobProps) => {
       className="bg-white rounded p-4 flex card transition-shadow items-center border hover:shadow-md"
       href={jobDescriptionLink}>
       <div className="flex gap-4 items-center w-full">
-        <div className="w-[72px] h-[72px] relative">
-          <Image
-            src={job.logo}
-            width={72}
-            height={72}
-            alt="Company logo"
-            style={{ objectFit: "cover", width: "100%", height: "auto" }}
-          />
+        <div className="w-[72px] h-[72px] relative flex items-center justify-center">
+          {job.logo ? (
+            <Image
+              src={job.logo}
+              width={72}
+              height={72}
+              alt="Company logo"
+              style={{ objectFit: "cover", width: "100%", height: "auto" }}
+            />
+          ) : (
+            <ImageOff size={50} />
+          )}
         </div>
         <div className="flex flex-1 flex-col md:flex-row justify-between gap-1">
           <div className="flex flex-col gap-1 items-start">
             <h2 className="font-semibold text-sm sm:text-[1.125rem]">{job.role}</h2>
             <p className="font-medium text-sm">{job.company}</p>
-            <div className="flex gap-2">
+            <div className="flex gap-2 items-center">
               <p className="text-sm text-[#1a0c6d] font-medium bg-[#1a0c6d]/10 px-2 rounded-xl border">
                 {job.type}
               </p>
-              {job.category && (
+              {job.category && job.category !== "N/A" && (
                 <p
                   className={cn(
                     "text-sm text-[#1a0c6d] font-medium px-2 rounded-xl border",
@@ -62,12 +66,14 @@ const Card = ({ job }: JobProps) => {
               )}
             </div>
           </div>
-          <div>
-            <p className="text-sm flex font-medium items-center gap-1">
-              <MapPin className="text-[#1a0c6d] size-4" />
-              {job.location}
-            </p>
-          </div>
+          {job.location && job.location !== "N/A" && (
+            <div>
+              <p className="text-sm flex font-medium items-center gap-1">
+                <MapPin className="text-[#1a0c6d] size-4" />
+                {job.location}
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </Link>

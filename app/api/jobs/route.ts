78d7352f5f4904ee11b07b24472be3a480b85d79
gulshanpr <gulshankumarprasad04@gmail.com>
2024-11-12 23:38:20 +0530

    removed caching issue

--- app/api/jobs/route.ts
+++ app/api/jobs/route.ts
@@ -2,13 +2,16 @@ import connectToMongoose from "@/db/connection";
 import Job from "@/models/job";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
 export async function GET() {
   try {
     await connectToMongoose();
 
     const jobs = await Job.find();
 
-    return NextResponse.json({ status: 200, jobs });
+    const response = NextResponse.json({ status: 200, jobs });
+    response.headers.set("Cache-Control", "no-store");
+    return response;
   } catch (error) {
     console.error("Error fetching jobs:", error);
     return NextResponse.json(
